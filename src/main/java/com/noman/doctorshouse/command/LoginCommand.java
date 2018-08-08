970f1a6e3c8588f80d11e6b66db5513a95ffd2b3
Noman46 <nomanibrahim7@gmail.com>
2018-08-09 03:31:41 +0600

    Login command class is done

---
+++ src/main/java/com/noman/doctorshouse/command/LoginCommand.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.noman.doctorshouse.command;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+public class LoginCommand {
+    
+    private String studentId;
+    private String password;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
