3337e2374a986afd6c8467bace317d42253ddc1d
Noman46 <nomanibrahim7@gmail.com>
2018-08-12 02:39:57 +0600

    Prescription Data access Object and Its implementation is done

---
+++ src/main/java/com/noman/doctorshouse/dao/PrescriptionDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.noman.doctorshouse.dao;
+
+import com.noman.doctorshouse.domain.Prescription;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+public interface PrescriptionDao {
+    public void save(Prescription pre);
+    public Prescription findById(String studentId);
+}
---
+++ src/main/java/com/noman/doctorshouse/dao/PrescriptionDaoImpl.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.noman.doctorshouse.dao;
+
+import com.noman.doctorshouse.domain.Prescription;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Noman Ibrahim
+ */
+@Repository
+public class PrescriptionDaoImpl extends BaseDao implements PrescriptionDao{
+
+    @Override
+    public void save(Prescription pre) {
+        String sql = "INSERT INTO prescription(doctorId, studentId, studentName, age, disease, medicine, strength, route, frequency, startDate, endDate, remarks)"
+                + " VALUES(:doctorId, :studentId, :studentName, :age, :disease, :medicine, :strength, :route, :frequency, :startDate, :endDate, :remarks)";
+        Map m = new HashMap();
+        m.put("doctorId", pre.getDoctorId());
+        m.put("studentId", pre.getStudentId());
+        m.put("studentName", pre.getStudentName());
+        m.put("age", pre.getAge());
+        m.put("disease", pre.getDisease());
+        m.put("medicine", pre.getMedicine());
+        m.put("strength", pre.getStrength());
+        m.put("route", pre.getRoute());
+        m.put("frequency", pre.getFrequency());
+        m.put("startDate", pre.getStartDate());
+        m.put("endDate", pre.getEndDate());
+        m.put("remarks", pre.getRemarks());
+       
+
+        KeyHolder kh = new GeneratedKeyHolder();
+        SqlParameterSource ps = new MapSqlParameterSource(m);
+        super.getNamedParameterJdbcTemplate().update(sql, ps, kh);
+        Integer preId = kh.getKey().intValue();
+        pre.setPreId(preId);
+    }
+
+    @Override
+    public Prescription findById(String studentId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
